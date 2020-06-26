3d927d5ee72fcb32e538fc6a4cbe7ed37c857a9d
heweiqi123 <123610105@qq.com>
2020-06-27 01:51:31 +0800

    ..

--- README.md
+++ README.md
@@ -1,2 +1,90 @@
 # SpeechDemo
-语音识别
+
+## 简介
+
+> 对现场或预先录制的音频进行语音识别,并接收回调的结果。
+
+## SDKs
+
+* iOS 10.0+
+* macOS 10.15+
+* Mac Catalyst 13.0+
+
+## 运行环境
+
+* Xcode 11.5
+* iOS 10.0及以上
+
+## 工程配置
+
+工程中必须在Info.plist文件中添加以下键值，否则会崩溃
+
+```Objective-C
+    <key>NSSpeechRecognitionUsageDescription</key>
+    <string>音吧需要使用语音识别功能来为您服务</string>
+```
+
+## API介绍
+
+```Objective-C
+
+    #import <Speech/SFVoiceAnalytics.h> //录制音频的语音分析类
+    #import <Speech/SFSpeechRecognitionResult.h> //请求结果类
+    #import <Speech/SFSpeechRecognitionRequest.h> //请求类，包含处理现有音频和音频流的请求类
+    #import <Speech/SFSpeechRecognitionTask.h> //监控语音识别进程的任务对象类
+    #import <Speech/SFSpeechRecognitionTaskHint.h> //语音识别的任务类型
+    #import <Speech/SFSpeechRecognizer.h> //用于检查语音识别服务的可用性并启动语音识别过程的对象。
+    #import <Speech/SFTranscriptionSegment.h> //语音识别转换信息的一小段，由speech Recognizer提供
+    #import <Speech/SFTranscription.h> //由speech Recognizer给定的语音的完整文本表示
+
+```
+
+```Objective-C
+typedef NS_ENUM(NSInteger, SFSpeechRecognizerAuthorizationStatus) {
+    SFSpeechRecognizerAuthorizationStatusNotDetermined,
+    //用户已授权
+    SFSpeechRecognizerAuthorizationStatusDenied,
+    //用户已拒绝
+    SFSpeechRecognizerAuthorizationStatusRestricted,
+    //该设备限制语音识别服务
+    SFSpeechRecognizerAuthorizationStatusAuthorized,
+    //没有授权
+} API_AVAILABLE(ios(10.0), macos(10.15));
+```
+
+```Objective-C
+typedef NS_ENUM(NSInteger, SFSpeechRecognitionTaskHint) {
+    SFSpeechRecognitionTaskHintUnspecified = 0,     
+    /* 
+        Unspecified recognition
+        未指定的任务类型。
+    */
+    SFSpeechRecognitionTaskHintDictation = 1,       
+    /* 
+        General dictation/keyboard-style
+        使用捕获的语音进行文本输入的任务。
+    */
+    SFSpeechRecognitionTaskHintSearch = 2,
+    /*
+        Search-style requests
+        使用捕获的语音来指定搜索术语的任务。
+    */
+    SFSpeechRecognitionTaskHintConfirmation = 3,    
+    /*
+        Short, confirmation-style requests ("Yes", "No", "Maybe")
+        捕获的语音用于简短的、确认式的请求。
+    */
+} API_AVAILABLE(ios(10.0), macos(10.15));
+```
+
+
+## 注意事项
+
+* Speech框架必须iOS 10.0以后才能使用，包含10.0
+* 现有音频文件使用SFSpeechURLRecognitionRequest对象,音频流使用SFSpeechAudioBufferRecognitionRequest对象
+* 语音识别不能超过一分钟
+
+
+## 参考资料
+
+[官网speech](https://developer.apple.com/documentation/speech)
--- SFSpeechDemo/SFSpeechDemo/ViewController.m
+++ SFSpeechDemo/SFSpeechDemo/ViewController.m
@@ -7,9 +7,9 @@
 //
 
 #import "ViewController.h"
-#import <Speech/SFSpeechRecognizer.h>
+#import <Speech/Speech.h> //管理语音识别过程的上下文对象
 
-@interface ViewController ()
+@interface ViewController ()<SFSpeechRecognizerDelegate>
 
 @end
 
@@ -18,6 +18,86 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self speechRecognizer];
+}
+
+/*
+    注意事项:
+    1.Speech框架必须iOS 10.0以后才能使用，包含10.0
+    2.现有音频文件使用SFSpeechURLRecognitionRequest对象,音频流使用SFSpeechAudioBufferRecognitionRequest对象
+    3.语音识别不能超过一分钟
+ 
+    流程:
+    1.请求语音识别的授权
+      在plist文件中增加keyNSSpeechRecognitionUsageDescription,不然会崩溃
+    2.创建SFSpeechRecognizer对象
+    3.根据isAvailable属性检查语音识别服务是否可以对当前语音使用
+    4.准备音频内容
+    5.创建一个请求识别对象（SFSpeechRecognitionRequest）
+    6.调用recognitionTask(with:delegate:) 或者 recognitionTask(with:resultHandler:) 方法开始识别过程
+ */
+
+- (void)speechRecognizer
+{
+    SFSpeechRecognizer *spR = [[SFSpeechRecognizer alloc] init];
+    spR.delegate = self;
+    
+    for (NSLocale *locale in [SFSpeechRecognizer supportedLocales]) {
+        NSLog(@"languageCode = %@", locale.languageCode);
+    }
+
+    //该方法为异步方法,用户第一次使用语音识别服务时，系统会记录用户的选择，之后的请求会立即返回之前记录的结果
+    [SFSpeechRecognizer requestAuthorization:^(SFSpeechRecognizerAuthorizationStatus status) {
+        switch (status) {
+            case SFSpeechRecognizerAuthorizationStatusAuthorized:
+            {
+                //用户已授权
+                NSLog(@"用户已授权");
+                if (spR.isAvailable) { //检查语音识别服务是否可以对当前语音使用，否则需要联网
+                    SFSpeechAudioBufferRecognitionRequest *rRequest = [[SFSpeechAudioBufferRecognitionRequest alloc] init];
+                    [spR recognitionTaskWithRequest:rRequest resultHandler:^(SFSpeechRecognitionResult * _Nullable result, NSError * _Nullable error) {
+                        if (result) {
+                            NSLog(@"result = %@", result);
+                        }
+                    }];
+                }
+            }
+                break;
+            case SFSpeechRecognizerAuthorizationStatusDenied:
+            {
+                //用户已拒绝
+                NSLog(@"用户已拒绝");
+            }
+                break;
+            case SFSpeechRecognizerAuthorizationStatusRestricted:
+            {
+                //该设备限制语音识别服务
+                NSLog(@"该设备限制语音识别服务");
+            }
+                break;
+            case SFSpeechRecognizerAuthorizationStatusNotDetermined:
+            {
+                //语音识别没有授权
+                NSLog(@"语音识别没有授权");
+            }
+                break;
+            default:
+                break;
+        }
+    }];
+}
+
+#pragma mark -- SFSpeechRecognizerDelegate
+- (void)speechRecognizer:(SFSpeechRecognizer *)speechRecognizer availabilityDidChange:(BOOL)available
+{
+    if (available) { //检查语音识别服务是否可以对当前语音使用，否则需要联网
+        
+    }
 }
 
 
